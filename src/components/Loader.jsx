d8cac14d7cf3b9c8eb9e47feca12a21b9131a511
Andrew Lenhart <andrew.code21@gmail.com>
2021-11-28 03:22:16 -0500

    Loading page v1

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import anime from 'animejs'
 import { BrandLoader } from '../assets/logos';
 
 const StyledLoader = styled.div`
@@ -13,19 +14,48 @@ const StyledLoader = styled.div`
   .logo__wrapper {
     max-width: 100px;
     svg {
+      display: block;
       width: 100%;
       height: 100%;
       margin: 0 auto;
+    }
+
+    h1 {
+      ${({ theme }) => theme.mixins.flexCenter};
+      color: var(--orange);
+      font-family: var(--font-link);
+      margin-top: 20px;
+      font-size: var(--text);
+      letter-spacing: 7px;
+      fon
     }
   }
 `;
 
 const Loader = () => {
+  const animate = () => {
+    const loader = anime.timeline({
+      duration: 1750,
+      loop: true
+    });
+  
+    loader
+      .add({
+        targets: '#logo',
+        delay: 750,
+        rotate: 360
+    })
+  }
+
+  useEffect(() => {
+    animate();
+  }, [])
 
   return (
     <StyledLoader>
       <div className="logo__wrapper">
         <BrandLoader />
+        <h1>WELCOME</h1>
       </div>
     </StyledLoader>
   )
