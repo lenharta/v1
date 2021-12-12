8a9008b5b659650e826fde257bed3969292db5bd
Andrew Lenhart <andrew.code21@gmail.com>
2021-12-12 01:30:40 -0500

    Update FeaturedProject.jsx

--- src/components/FeaturedProject.jsx
+++ src/components/FeaturedProject.jsx
@@ -10,6 +10,16 @@ const StyledProject = styled.div`
   grid-template-columns: repeat(12, 1fr);
   color: var(--text);
 
+  &:not(:last-of-type) {
+    margin-bottom: 100px;
+    @media (max-width: 768px) {
+      margin-bottom: 70px;
+    }
+    @media (max-width: 480px) {
+      margin-bottom: 30px;
+    }
+  }
+
   .project__img {
     position: relative;
     grid-column: 1 / 8;
@@ -19,6 +29,14 @@ const StyledProject = styled.div`
     border-radius: 15px;
     overflow: hidden;
     z-index: 1;
+
+    @media (max-width: 1080px) {
+      grid-column: 10 / 1;
+    }
+
+    @media (max-width: 768px) {
+      grid-column: 1 / -1;
+    }
     
     &:before {
       position: absolute;
@@ -29,15 +47,24 @@ const StyledProject = styled.div`
       right: 0;
       z-index: 3;
       background: var(--orange);
-      opacity: 0.4;
+      opacity: 0.6;
+      filter: grayscale(15%);
       &:hover {
-        background: transparent;
+        background: none;
+      }
+      @media (max-width: 768px) {
+        background: var(--bg-navy);
+        opacity: 0.95;
       }
     }
 
     img {
       width: 100%;
       height: 100%;
+      @media (max-width: 768px) {
+        object-fit: cover;
+        filter: grayscale(100%) contrast(1);
+      }
     }
   }
 
@@ -50,6 +77,18 @@ const StyledProject = styled.div`
     grid-row: 1 / -1;
     text-align: right;
     z-index: 10;
+
+    @media (max-width: 1080px) {
+      grid-column: 5 / -1;
+    }
+    @media (max-width: 768px) {
+      grid-column: 1 / -1;
+      padding: 40px 40px 30px;
+      text-align: left;
+    }
+    @media (max-width: 480px) {
+      padding: 25px 25px 20px;
+    }
 
     .featured__title {
       font-family: var(--font-link);
@@ -74,12 +113,22 @@ const StyledProject = styled.div`
       padding: 20px;
       margin-bottom: 20px;
       border-radius: 15px;
+
+      @media (max-width: 768px) {
+        background: none;
+        padding: 0;
+      }
     }
 
     .project__tech {
       font-family: var(--font-link);
       font-size: 14px;
       color: var(--orange);
+
+      @media (max-width: 768px) {
+        font-size: 10px;
+        padding-top: 10px
+      }
     }
 
     .links {
@@ -88,11 +137,22 @@ const StyledProject = styled.div`
       justify-content: flex-end;
       font-family: var(--font-link);
       transform: translateX(25px);
+
+      @media (max-width: 768px) {
+        justify-content: flex-start;
+        transform: translateX(-25px);
+        margin-top: 20px;
+      }
+
       a {
         padding: 25px;
         svg {
           width: 40px;
           height: 40px;
+          @media (max-width: 768px) {
+            width: 30px;
+            height: 30px;
+          }
         }
       }
     }
