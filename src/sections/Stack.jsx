d029754fcde6a05a75d9102455939e8d52efd356
Andrew Lenhart <andrew.code21@gmail.com>
2022-01-10 20:29:53 -0500

    stack-update

--- src/sections/Stack.jsx
+++ src/sections/Stack.jsx
@@ -10,91 +10,136 @@ const StyledStackContainer = styled.section`
   ${({ theme }) => theme.mixins.flexCenter};
   flex-direction: column;
   max-width: 1000px;
-
-  .perspective {
-    perspective: 1000px;
-    
-    @media (max-width: 480px) {
-      perspective: 0;
-    }
-  }
-
-  .section__heading-top {
-    margin-bottom: 10px;
-  }
+  padding-bottom: 0;
 `;
 
 const StyledStackInner = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(6, 1fr);
-  grid-gap: 16px;
+  padding: 50px 0;
   width: 100%;
-  max-height: 700px; 
-  padding: 0 150px;
-
-  transform: rotateX(35deg);
-
-  @media (max-width: 768px) {
-    grid-gap: 6px;
-    max-width: 600px;
-    padding: 0 25px;
-  }
 
   @media (max-width: 480px) {
-    transform: rotateX(0deg);
     padding: 25px 0;
   }
 
-  .card {
-    height: 100%;
-    width:100%;
-    
-    
-    div {
-      ${({ theme }) => theme.mixins.flexCenter};
-      background: linear-gradient(180deg, rgba(14, 18, 25, 1) 0%, rgba(22, 28, 39, 1) 100%);
-      border-radius: 15px;
-      border-bottom-left-radius: 25px;
-      border-bottom-right-radius: 25px;
-      height: 100%;
+  .title {
+    overflow-wrap: break-word;
+    width: 50%;
+    font-size: clamp(var(--fs-xs), 5vw, var(--fs-md));
+    color: var(--text);
+    font-family: var(--font-link);
+
+    @media (max-width: 480px) {
       width: 100%;
-      border-top: solid .5px var(--orange-tint);
-      border-left: solid 1px var(--orange-tint);
-      border-right: solid 1px var(--orange-tint);
-      border-bottom: solid 10px var(--orange-tint);
-      @media (max-width: 480px) {
-        border: solid 1px var(--orange-tint);
-        border-radius: 15px;
-      }
-      
-      svg {
-        height: 75%;
-        width: 75%; 
-      }
-
-      &:hover,
-      &:focus-visible {
-        box-shadow: 0 0 32px var(--orange-tint);
-        transform: scale(1.05);
-        transform: translateY(-20px);
-        transition: var(--transition);
-      }
     }
-
-    &:nth-child(1), &:nth-child(4), &:nth-child(5), &:nth-child(7) {
-      grid-row: span 3;
-    }
-
-    &:nth-child(2), &:nth-child(3) {
-      grid-row: span 2;
-    }
-
-    &:nth-child(6) {
-      grid-column: 2 / span 2;
-      grid-row: 3 / span 4;
+    .divider {
+      width: 75px;
+      height: 3px;
+      background: var(--gradient-text);
+      margin-bottom: 15px;
     }
   }
+
+  ul {
+    background: var(--bg-navy);
+    border-radius: 15px;
+  }
+`;
+
+const ProgrammingLangs = styled.div`
+  ${({ theme }) => theme.mixins.flexBetween};
+  align-items: flex-start;
+  flex-direction: column;
+  width: 100%;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+
+  ul {
+    width: 100%;
+    ${({ theme }) => theme.mixins.flexCenter};
+    flex-direction: column;
+    padding: 30px;
+    margin-top: 50px;
+
+    @media (max-width: 600px) {
+      margin-top: 25px;
+      width: 100%;
+    }
+
+    li {
+      ${({ theme }) => theme.mixins.flexBetween};
+      width: 100%;
+      margin-bottom: 20px;
+      font-size: var(--fs-sm);
+      color: var(--text);
+      font-family: var(--font-text);
+      text-align: left;
+      &:last-of-type {
+        margin: 0;
+      }
+      svg {
+        height: 50px;
+        width: 50px;
+      }
+    }
+  }
+`;
+
+const StyledStackInnerSub = styled.div`
+  ${({ theme }) => theme.mixins.flexBetween};
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  margin-top: 75px;
+  @media (max-width: 480px) {
+    margin-top: 50px
+  }
+
+  ul {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    margin: 50px 0;
+    padding: 30px;
+
+    @media (max-width: 768px) {
+      margin-top: 20px;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 10px;
+      padding: 30px;
+    }
+
+    @media (max-width: 600px) {
+      grid-template-columns: repeat(2, 1fr);
+      grid-gap: 10px;
+    }
+
+    @media (max-width: 350px) {
+      grid-template-columns: repeat(1, 1fr);
+      grid-gap: 5px;
+    }
+
+    li {
+      ${({ theme }) => theme.mixins.flexBetween};
+      width: 100%;
+      font-size: clamp(var(--fs-xs), 5vw, var(--fs-sm));
+      color: var(--text);
+      font-family: var(--font-text);
+      text-align: left;
+    }
+  }
+`;
+
+const Libraries = styled.div`
+  width: 100%;
+  display: grid;
+`;
+
+const Tools = styled.div`
+  width: 100%;
+  display: grid;
 `;
 
 const Stack = () => {
@@ -109,75 +154,90 @@ const Stack = () => {
     sr.reveal(revealContainer.current, srConfig())
   }, [])
 
-  const one = (
-    <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" aria-label="HTML 5 Link">
-      <div>
-        <Icons name="HTML5" />
-      </div>
-    </a>
+  const program1 = (
+    <>
+      <p>HTML5</p>
+      <Icons name="HTML5" />
+    </>
   )
 
-  const two = (
-    <a href="https://sass-lang.com/" aria-label="Sass Link">
-      <div>
-        <Icons name="Sass" />
-      </div>
-    </a>
-  )
-  
-  const three = (
-    <a href="https://www.figma.com/" aria-label="Figma Link">
-      <div>
-        <Icons name="Figma" />
-      </div>
-    </a>
-  )
-  
-  const four = (
-    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" aria-label="CSS 3 Link">
-      <div>
-        <Icons name="Css3" />
-      </div>
-    </a>
-  )
-  
-  const five = (
-    <a href="https://www.javascript.com/" aria-label="Javascript Link">
-      <div>
-        <Icons name="JavaScript" />
-      </div>
-    </a>
-  )
-  
-  const six = (
-    <a href="https://reactjs.org/" aria-label="React Link">
-      <div>
-        <Icons name="React" />
-      </div>
-    </a>
-  )
-  
-  const seven = (
-    <a href="https://redux.js.org/" aria-label="Redux Link">
-      <div>
-        <Icons name="Redux" />
-      </div>
-    </a>
+  const program2 = (
+    <>
+      <p>CSS3</p>
+      <Icons name="Css3" />
+    </>
   )
 
-  const stackItems = [one, two, three, four, five, six, seven]
+  const program3 = (
+    <>
+      <p>JavaScript</p>
+      <Icons name="JavaScript" />
+    </>
+  )
+  
+  const program4 = (
+    <>
+      <p>React.js</p>
+      <Icons name="React" />
+    </>
+  )
 
+  const program5 = (
+    <>
+      <p>Sass</p>
+      <Icons name="Sass" />
+    </>
+  )
+
+  
+  const progLangItems = [program1, program2, program3, program4, program5]
+  const libraryItems = ["Gatsby", "Axios", "Node.js", "Anime.js", "ScrollReveal", "MaterialUI", "Framer Motion", "React Router"]
+  const toolItems = ["Git", "Github", "Webpack", "Netlify", "Firebase", "Figma"]
+
+  
   return (
     <>
       <StyledStackContainer id="stack" ref={revealContainer} >
         <h2 className="section__heading-top">Stack</h2>
-        <div className="perspective">
-        <StyledStackInner >
-          {stackItems.map((item, i) => (
-            <div key={i} className="card">{item}</div>
-          ))}
+        <StyledStackInner>
+          <ProgrammingLangs>
+            <div className="title">
+              <div className="divider"></div>
+              <h2>Programming Languages</h2>
+            </div>
+            <ul>
+              {progLangItems.map((item, i) => (
+                <li key={i} className="list-item">{item}</li>
+              ))}
+            </ul>
+          </ProgrammingLangs>
+
+          <StyledStackInnerSub>
+            <Libraries>
+              <div className="title">
+                <div className="divider"></div>
+                <h2>Libraries & Frameworks</h2>
+              </div>
+              <ul>
+                {libraryItems.map((item, i) => (
+                  <li key={i} className="list-item">{item}</li>
+                ))}
+              </ul>
+            </Libraries>
+
+            <Tools>
+              <div className="title">
+                <div className="divider"></div>
+                <h2>Tools & Platforms</h2>
+              </div>
+              <ul>
+                {toolItems.map((item, i) => (
+                  <li key={i} className="list-item">{item}</li>
+                ))}
+              </ul>
+            </Tools>
+          </StyledStackInnerSub>
         </StyledStackInner>
-        </div>
       </StyledStackContainer>
     </>
   )
